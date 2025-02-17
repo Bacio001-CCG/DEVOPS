bbe9adc362edcb340e587850a12a9e9de2c0e1ab
Bacio001-CCG <bacio0011@gmail.com>
2025-02-17 15:27:45 +0100

    Add initial project structure with Docker setup and Express routes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DEVOPS
+
+App Commands:
+
+- npm run docker:dev
+- npm run docker:dev
+-
+
+DB Commands:
+
+- docker compose -f docker-compose.yml up --build
---
+++ members/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+
+import indexRouter from './routes/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use('/', indexRouter);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
+
+export default app;
