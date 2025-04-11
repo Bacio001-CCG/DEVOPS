f604315a13333f863a463c35edf40ca678b29d91
PaulBerkersAvans <130966922+PaulBerkersAvans@users.noreply.github.com>
2025-04-11 15:07:02 +0200

    fixed auth rabbidmq

--- services/auth/src/rabbitmq.js
+++ services/auth/src/rabbitmq.js
@@ -6,6 +6,12 @@ class RabbitMQClient {
   channel = null;
   queues = [];
 
+  /**
+   * Creates a new RabbitMQClient instance
+   * @param {Array<{queue: string, function: function(import('amqplib').ConsumeMessage): void}>} queues - Array of queue configurations
+   * @param {string} queues[].queue - Name of the queue
+   * @param {function(import('amqplib').ConsumeMessage): void} queues[].function - Callback function to process messages
+   */
   constructor(queues = []) {
     this.queues = queues;
     this.initialize();
@@ -13,29 +19,26 @@ class RabbitMQClient {
 
   async initialize() {
     try {
-      this.connection = await amqp.connect(process.env.RABBITMQ_URL || "amqp://localhost");
+      this.connection = await amqp.connect(process.env.RABBITMQ_URL);
       console.log("Connected to RabbitMQ");
 
       this.channel = await this.connection.createChannel();
       console.log("Channel created successfully");
 
-      // Only set up consumers if queues are provided
-      if (this.queues.length > 0) {
-        for (const queue of this.queues) {
-          await this.channel.assertQueue(queue.queue, { durable: false });
-          console.log("Queue asserted:", queue.queue);
+      for (const queue of this.queues) {
+        await this.channel.assertQueue(queue.queue, { durable: false });
+        console.log("Queue asserted:", queue.queue);
 
-          this.channel.consume(
-            queue.queue,
-            (msg) => {
-              if (msg !== null) {
-                queue.function(msg);
-              }
-            },
-            { noAck: true }
-          );
-          console.log("Consumer set up for queue:", queue.queue);
-        }
+        this.channel.consume(
+          queue.queue,
+          (msg) => {
+            if (msg !== null) {
+              queue.function(msg);
+            }
+          },
+          { noAck: true }
+        );
+        console.log("Consumer set up for queue:", queue.queue);
       }
 
       console.log("RabbitMQ setup complete");
@@ -51,7 +54,6 @@ class RabbitMQClient {
         return;
       }
 
-      await this.channel.assertQueue(queue, { durable: false });
       this.channel.sendToQueue(queue, Buffer.from(msg), { persistent: true });
       console.log(" [x] Sent '%s' to queue '%s'", msg, queue);
     } catch (error) {
